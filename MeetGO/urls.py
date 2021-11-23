919fdd6a5f1fc39205d53551a492ea01ce3018eb
Ali Yaman <aliymn.db@gmail.com>
2021-11-23 15:24:06 +0300

    template

--- MeetGO/urls.py
+++ MeetGO/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-from go.views import homePage
+from go.views import homePage,exploreMeetings,meetingDetails
 from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homePage),
+    path('details/', meetingDetails),
+    path('explore/', exploreMeetings),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- go/views.py
+++ go/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
 def homePage(request):
     return render(request,'index.html')
+
+def meetingDetails(request):
+    return render(request,'details.html')
+
+def exploreMeetings(request):
+    return render(request,'explore.html')
