57153be69bdb50e8303bf0c68665d729de13a006
Ali Yaman <aliymn.db@gmail.com>
2021-12-11 23:01:13 +0300

    heroku settings.py update

--- MeetGO/settings.py
+++ MeetGO/settings.py
@@ -80,15 +80,16 @@ WSGI_APPLICATION = 'MeetGO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
