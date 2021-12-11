8fc73f17bf5062cd88417ddc0860d94aad74f569
Ali Yaman <aliymn.db@gmail.com>
2021-12-11 22:30:36 +0300

    heroku settings.py

--- MeetGO/settings.py
+++ MeetGO/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,3 +157,6 @@ COMPRESS_OFFLINE = True
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
